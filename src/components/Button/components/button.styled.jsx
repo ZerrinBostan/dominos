80ea9ddad931ed699568975242b8b55675b9ce76
Zerrin <zerrinbstn@gmail.com>
2021-03-01 13:33:56 +0300

    Button structure is established.

---
+++ src/components/Button/components/button.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const ButtonItem = styled.div`
+  width: 100%;
+  padding: 10px 0px;
+  font-size: 12px;
+  font-weight: 600;
+  border-radius: 100px;
+  line-height: 1;
+  background: white;
+  border: 1px solid;
+  cursor: pointer;
+`;
+export default ButtonItem;
